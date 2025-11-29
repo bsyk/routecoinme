444b91895424e5bf5fa2588116a505c4943fe901
Ben Sykes <ben@bensykes.com>
2025-11-29 12:53:33 -0800

    Cleanup to use Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   root: '.',
-  base: process.env.NODE_ENV === 'production' ? '/routecoinme/' : '/',
+  base: '/',
   server: {
     port: 3000,
     open: true,
