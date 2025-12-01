989cc53984c359ea4d7af1b8050f7a46e0ffa781
Ben Sykes <ben@bensykes.com>
2025-12-01 10:26:38 -0800

    Fix static pages

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite'
 import { cloudflare } from "@cloudflare/vite-plugin";
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   plugins: [cloudflare()],
@@ -10,9 +14,20 @@ export default defineConfig({
     open: true,
     host: true
   },
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: true
+  environments: {
+    client: {
+      build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: true,
+        rollupOptions: {
+          input: {
+            main: resolve(__dirname, 'index.html'),
+            terms: resolve(__dirname, 'terms/index.html'),
+            privacy: resolve(__dirname, 'privacy/index.html'),
+          },
+        },
+      }
+    }
   }
 })
